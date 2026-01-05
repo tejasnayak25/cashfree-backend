70e83a181e5519cfc15cb44e0ac164321f3504af
tejasnayak25 <arpithanayak900@gmail.com>
2026-01-05 19:43:09 +0530

    fix: update rate limit to restrict each IP to 10 requests per window

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const expressrateLimit = require('express-rate-limit');
 
 const limiter = expressrateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 10, // limit each IP to 10 requests per windowMs
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the deprecated `X-RateLimit-*` headers
     message: 'Too many requests, please try again later.'
